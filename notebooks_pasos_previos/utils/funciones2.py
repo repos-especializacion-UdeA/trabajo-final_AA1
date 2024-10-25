459915a37e870e3a14201bda12e3da0037331887
Henry Arcila <helpu1409@gmail.com>
2024-10-25 17:11:58 -0500

    Actualizando repo tarea final

---
+++ notebooks_pasos_previos/utils/funciones2.py
@@ -0,0 +1,12 @@
+#Librerias
+import math
+import numpy as np
+import pandas as pd
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Funciones de prueba
+
+def test_hola():
+    print("Hola amigos")
